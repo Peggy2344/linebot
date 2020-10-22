1a8b2e62ce67c451923b98f37772b422c515d879
Peggy2344 <peggy102104@gmail.com>
2020-10-22 23:22:57 +0800

    修改bug

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import linebot from 'linebot'
 // 引用dotenv套件
 import dotenv from 'dotenv'
 import axios from 'axios'
+import cheerio from 'cheerio'
 
 // 讀取 .env
 dotenv.config()
-
 // 設定機器人
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
@@ -14,70 +14,17 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
-bot.on('message', async event => {
-  try {
-    const response = await axios.get('https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6')
-    const text = event.message.text
-    let reply = ''// 引用line機器人套件
-    import linebot from 'linebot'
-    // 引用dotenv套件
-    import dotenv from 'dotenv'
-    import axios from 'axios'
-    import cheerio from 'cheerio'
-
-    // import schedule from 'node-schedule'
-
-    // const url = `https://dict.longdo.com/mobile.php?search=${text}`
-    // const encode = encodeURI(url)
-    // const updateData = async () => {
-    //   const response = await axios.get(encode)
-    //   const $ = cheerio.load(response.data)
-    // }
-
-    // schedule.scheduleJob('* * 0 * * * ', () => {
-    //   updateData()
-    // })
-
-    // updateData()
-    // 讀取 .env
-    dotenv.config()
-
-    // 設定機器人
-    const bot = linebot({
-      channelId: process.env.CHANNEL_ID,
-      channelSecret: process.env.CHANNEL_SECRET,
-      channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
-    })
-
-    // bot.on('message', async event => {
-    //   try {
-    //     const text = event.message.text
-    //     let reply = ''
-    //     for (const data of exhibitions) {
-    //       if (data.title === text) {
-    //         reply = data.showInfo[0].locationName
-    //         break
-    //       }
-    //     }
-    //     reply = (reply.length === 0) ? '沒有此筆資料' : reply
-    //     event.reply(reply)
-    //   } catch (error) {
-    //     event.reply('發生錯誤')
-    //   }
-    // })
-
-    // bot.listen('/', process.env.PORT, () => {
-    //   console.log('機器人已啟動')
-    // })
-
     bot.on('message', async event => {
       try {
         const text = event.message.text
         const url = `https://dict.longdo.com/mobile.php?search=${text}`
         const encode = encodeURI(url)
+        const $ = ''
         const updateData = async () => {
           const response = await axios.get(encode)
-          const $ = cheerio.load(response.data)
+          $ = cheerio.load(response.data)
+        }
+        updateData()
           let reply = ''
           const result = $('b').filter(function (i, el) {
             return $(el).text() === 'NECTEC Lexitron-2 Dictionary (TH-EN)'
@@ -85,32 +32,14 @@ bot.on('message', async event => {
           const translate = result.slice(0, result.indexOf(',')).join('')
           const example = result.slice(result.lastIndexOf(':') + 2).join('')
           reply = translate + example
-          reply = (reply.length === 0) ? '沒有此筆資料' : reply
+          reply = (reply.length === 0) ? '嗨嗨' : reply
           event.reply(reply)
-        }
-        updateData()
+        
       } catch (error) {
-        event.reply('發生錯誤')
+        event.reply(error)
       }
     })
 
     bot.listen('/', process.env.PORT, () => {
       console.log('機器人已啟動')
     })
-
-    for (const data of response.data) {
-      if (data.title === text) {
-        reply = data.showInfo[0].locationName
-        break
-      }
-    }
-    reply = (reply.length === 0) ? '沒有此筆資料' : reply
-    event.reply(reply)
-  } catch (error) {
-    event.reply('發生錯誤')
-  }
-})
-
-bot.listen('/', process.env.PORT, () => {
-  console.log('機器人已啟動')
-})
