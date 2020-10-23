cd4ffbcc91a1236b781a69e00866d8759dba5b18
Peggy2344 <peggy102104@gmail.com>
2020-10-23 16:33:48 +0800

    更新

--- index.js
+++ index.js
@@ -14,32 +14,34 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
-    bot.on('message', async event => {
-      try {
-        const text = event.message.text
-        const url = `https://dict.longdo.com/mobile.php?search=${text}`
-        const encode = encodeURI(url)
-        const $ = ''
-        const updateData = async () => {
-          const response = await axios.get(encode)
-          $ = cheerio.load(response.data)
-        }
-        updateData()
-          let reply = ''
-          const result = $('b').filter(function (i, el) {
-            return $(el).text() === 'NECTEC Lexitron-2 Dictionary (TH-EN)'
-          }).next().find('tbody tr td').eq(1).children().remove('a').end().text().split('')
-          const translate = result.slice(0, result.indexOf(',')).join('')
-          const example = result.slice(result.lastIndexOf(':') + 2).join('')
-          reply = translate + example
-          reply = (reply.length === 0) ? '嗨嗨' : reply
-          event.reply(reply)
-        
-      } catch (error) {
-        event.reply(error)
-      }
-    })
+bot.on('message', async event => {
+  try {
+    const text = event.message.text
+    const url = `https://dict.longdo.com/mobile.php?search=${text}`
+    const encode = encodeURI(url)
+    let $ = ''
+    const updateData = async () => {
+      const response = await axios.get(encode)
+      $ = cheerio.load(response.data)
+      let reply = ''
+      const result = $('b').filter(function (i, el) {
+        return $(el).text() === 'NECTEC Lexitron-2 Dictionary (TH-EN)'
+      }).next().find('tbody tr td').eq(1).children().remove('a').end().text().split('')
+      const translate = result.slice(0, result.indexOf(',')).join('')
+      const example = result.slice(result.lastIndexOf('E'), result.lastIndexOf('T')).join('')
+      const thaiDef = result.slice(result.lastIndexOf('T')).join('')
+      reply = `${translate}
+      ${example}
+      ${thaiDef}`
+      reply = (reply.length === 0) ? '嗨嗨' : reply
+      event.reply(reply)
+    }
+    updateData()
+  } catch (error) {
+    event.reply('發生錯誤')
+  }
+})
 
-    bot.listen('/', process.env.PORT, () => {
-      console.log('機器人已啟動')
-    })
+bot.listen('/', process.env.PORT, () => {
+  console.log('機器人已啟動')
+})
