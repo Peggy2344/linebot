9f6f4c8aa1bbc7dbfe411a720a9fa75401dd2d9e
Peggy2344 <peggy102104@gmail.com>
2020-10-28 16:29:17 +0800

    newsapi

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 import axios from 'axios'
 import cheerio from 'cheerio'
 // import googleTTS from 'google-tts-api'
-import Gtts from 'gtts'
+// import Gtts from 'gtts'
 // import express from 'express'
 // import ffmpeg from 'fluent-ffmpeg'
 
@@ -18,140 +18,450 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
+const updateNews = async (catagory, event) => {
+  const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=th&category=${catagory}&apiKey=3e750ac7043b4a359c9cab5ad99e81c3`)
+  const articles = response.data.articles
+  let reply = ''
+  const imgUrl = 'https://images.pexels.com/photos/3944377/pexels-photo-3944377.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  const img1 = articles[0].urlToImage === null ? imgUrl : articles[0].urlToImage
+  const img2 = articles[1].urlToImage === null ? imgUrl : articles[1].urlToImage
+  const img3 = articles[2].urlToImage === null ? imgUrl : articles[2].urlToImage
+  reply = {
+    type: 'flex',
+    altText: 'Flex',
+    contents: {
+      type: 'carousel',
+      contents: [
+        {
+          type: 'bubble',
+          body: {
+            type: 'box',
+            layout: 'vertical',
+            contents: [
+              {
+                type: 'image',
+                url: img1,
+                size: 'full',
+                aspectMode: 'cover',
+                aspectRatio: '2:3',
+                gravity: 'top'
+              },
+              {
+                type: 'box',
+                layout: 'vertical',
+                contents: [
+                  {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: articles[0].title,
+                        wrap: true,
+                        size: 'xl',
+                        color: '#ffffff',
+                        weight: 'bold'
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                      {
+                        type: 'filler'
+                      },
+                      {
+                        type: 'box',
+                        layout: 'baseline',
+                        contents: [
+                          {
+                            type: 'filler'
+                          },
+                          {
+                            type: 'text',
+                            text: '查看新聞',
+                            color: '#ffffff',
+                            flex: 0,
+                            offsetTop: '-2px',
+                            contents: [
+                              {
+                                type: 'span',
+                                text: '查看新聞'
+                              }
+                            ]
+                          },
+                          {
+                            type: 'filler'
+                          }
+                        ],
+                        spacing: 'sm'
+                      },
+                      {
+                        type: 'filler'
+                      }
+                    ],
+                    borderWidth: '1px',
+                    cornerRadius: '4px',
+                    spacing: 'sm',
+                    borderColor: '#ffffff',
+                    margin: 'xxl',
+                    height: '40px',
+                    offsetTop: '20px',
+                    action: {
+                      type: 'uri',
+                      label: 'action',
+                      uri: articles[0].url
+                    }
+                  }
+                ],
+                position: 'absolute',
+                offsetBottom: '0px',
+                offsetStart: '0px',
+                offsetEnd: '0px',
+                backgroundColor: '#03303Acc',
+                paddingAll: '40px',
+                paddingTop: '40px'
+              }
+            ],
+            paddingAll: '0px'
+          }
+        },
+        {
+          type: 'bubble',
+          body: {
+            type: 'box',
+            layout: 'vertical',
+            contents: [
+              {
+                type: 'image',
+                url: img2,
+                size: 'full',
+                aspectMode: 'cover',
+                aspectRatio: '2:3',
+                gravity: 'top'
+              },
+              {
+                type: 'box',
+                layout: 'vertical',
+                contents: [
+                  {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: articles[1].title,
+                        wrap: true,
+                        size: 'xl',
+                        color: '#ffffff',
+                        weight: 'bold'
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                      {
+                        type: 'filler'
+                      },
+                      {
+                        type: 'box',
+                        layout: 'baseline',
+                        contents: [
+                          {
+                            type: 'filler'
+                          },
+                          {
+                            type: 'text',
+                            text: '查看新聞',
+                            color: '#ffffff',
+                            flex: 0,
+                            offsetTop: '-2px'
+                          },
+                          {
+                            type: 'filler'
+                          }
+                        ],
+                        spacing: 'sm'
+                      },
+                      {
+                        type: 'filler'
+                      }
+                    ],
+                    borderWidth: '1px',
+                    cornerRadius: '4px',
+                    spacing: 'sm',
+                    borderColor: '#ffffff',
+                    margin: 'xxl',
+                    height: '40px',
+                    offsetTop: '20px',
+                    action: {
+                      type: 'uri',
+                      label: 'action',
+                      uri: articles[1].url
+                    }
+                  }
+                ],
+                position: 'absolute',
+                offsetBottom: '0px',
+                offsetStart: '0px',
+                offsetEnd: '0px',
+                backgroundColor: '#9C8E7Ecc',
+                paddingAll: '40px',
+                paddingTop: '40px'
+              }
+            ],
+            paddingAll: '0px'
+          }
+        }, {
+          type: 'bubble',
+          body: {
+            type: 'box',
+            layout: 'vertical',
+            contents: [
+              {
+                type: 'image',
+                url: img3,
+                size: 'full',
+                aspectMode: 'cover',
+                aspectRatio: '2:3',
+                gravity: 'top'
+              },
+              {
+                type: 'box',
+                layout: 'vertical',
+                contents: [
+                  {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                      {
+                        type: 'text',
+                        text: articles[2].title,
+                        wrap: true,
+                        size: 'xl',
+                        color: '#ffffff',
+                        weight: 'bold'
+                      }
+                    ]
+                  },
+                  {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                      {
+                        type: 'filler'
+                      },
+                      {
+                        type: 'box',
+                        layout: 'baseline',
+                        contents: [
+                          {
+                            type: 'filler'
+                          },
+                          {
+                            type: 'text',
+                            text: '查看新聞',
+                            color: '#ffffff',
+                            flex: 0,
+                            offsetTop: '-2px',
+                            contents: [
+                              {
+                                type: 'span',
+                                text: '查看新聞'
+                              }
+                            ]
+                          },
+                          {
+                            type: 'filler'
+                          }
+                        ],
+                        spacing: 'sm'
+                      },
+                      {
+                        type: 'filler'
+                      }
+                    ],
+                    borderWidth: '1px',
+                    cornerRadius: '4px',
+                    spacing: 'sm',
+                    borderColor: '#ffffff',
+                    margin: 'xxl',
+                    height: '40px',
+                    offsetTop: '20px',
+                    action: {
+                      type: 'uri',
+                      label: 'action',
+                      uri: articles[2].url
+                    }
+                  }
+                ],
+                position: 'absolute',
+                offsetBottom: '0px',
+                offsetStart: '0px',
+                offsetEnd: '0px',
+                backgroundColor: '#03303Acc',
+                paddingAll: '40px',
+                paddingTop: '40px'
+              }
+            ],
+            paddingAll: '0px'
+          }
+        }
+      ]
+    }
+  }
+  event.reply(reply)
+}
+
+const updateData = async (dataUrl, event) => {
+  // const tts = new Gtts(`${text}`, 'th')
+  // tts.save('./sound/text.m4a', function (err, result) {
+  //   if (err) { throw new Error(err) }
+  //   console.log('Success! Open file to hear result.')
+  // })
+  // const app = express()
+  // app.get('./', function (req, res) {
+  //   const gtts = new Gtts(`${text}`, 'th')
+  //   gtts.stream().pipe(res)
+  //   console.log(res)
+  // })
+  const encode = encodeURI(dataUrl)
+  const response = await axios.get(encode)
+  const $ = cheerio.load(response.data)
+  let reply
+  const result = []
+  for (let i = 1; i <= 5; i += 2) {
+    const word = $('b').filter(function (i, el) {
+      return $(el).text() === 'NECTEC Lexitron-2 Dictionary (TH-EN)'
+    }).next().find('tbody tr td').eq(i).children().remove('a').end().text().split('')
+    console.log(word)
+    const translate = word.length > 0 ? word.slice(0, word.indexOf(',')).join('') : 'no results'
+    console.log(translate)
+    let example = ''
+    if (word.lastIndexOf('T') === -1) {
+      example = word.length > 0 ? word.slice(word.lastIndexOf('E') + 9).join('') : 'no results'
+    } else {
+      example = word.slice(word.lastIndexOf('E') + 9, word.lastIndexOf('T')).join('')
+    }
+    result.push({ translate: translate, example: example })
+  }
+  result.length === 0 ? reply = '沒有這個單字的資料，請搜尋其他單詞~'
+    : reply = [{
+      type: 'flex',
+      altText: 'Flex',
+      contents: {
+        type: 'carousel',
+        contents: [
+          {
+            type: 'bubble',
+            header: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [{
+                type: 'text',
+                wrap: true,
+                text: result[0].translate
+              }]
+            },
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [{
+                type: 'text',
+                wrap: true,
+                text: result[0].example
+              }]
+            },
+            styles: {
+              header: {
+                backgroundColor: '#83c5be'
+              }
+            }
+          },
+          {
+            type: 'bubble',
+            header: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [{
+                type: 'text',
+                wrap: true,
+                text: result[1].translate
+              }]
+            },
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [{
+                type: 'text',
+                wrap: true,
+                text: result[1].example
+              }]
+            },
+            styles: {
+              header: {
+                backgroundColor: '#83c5be'
+              }
+            }
+          }, {
+            type: 'bubble',
+            header: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [{
+                type: 'text',
+                wrap: true,
+                text: result[2].translate
+              }]
+            },
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [{
+                type: 'text',
+                wrap: true,
+                text: result[2].example
+              }]
+            },
+            styles: {
+              header: {
+                backgroundColor: '#83c5be'
+              }
+            }
+          }
+        ]
+      }
+    }]
+  event.reply(reply)
+}
 bot.on('message', async event => {
   try {
     const text = event.message.text
     const url = `https://dict.longdo.com/mobile.php?search=${text}`
-    const encode = encodeURI(url)
-    let $ = ''
-    // const news = ''
-    const updateData = async () => {
-      const tts = new Gtts(`${text}`, 'th')
-      tts.save('./text.m4a', function (err, result) {
-        if (err) { throw new Error(err) }
-        console.log('Success! Open file to hear result.')
-        console.log(result)
-      })
-      // const app = express()
-      // app.get('./', function (req, res) {
-      //   const gtts = new Gtts(`${text}`, 'th')
-      //   gtts.stream().pipe(res)
-      //   console.log(res)
-      // })
-      const response = await axios.get(encode)
-      $ = cheerio.load(response.data)
-      let reply
-      const result = []
-      for (let i = 1; i <= 5; i += 2) {
-        const word = $('b').filter(function (i, el) {
-          return $(el).text() === 'NECTEC Lexitron-2 Dictionary (TH-EN)'
-        }).next().find('tbody tr td').eq(i).children().remove('a').end().text().split('')
-        const translate = word.slice(0, word.indexOf(',')).join('')
-        let example = ''
-        if (word.lastIndexOf('T') === -1) {
-          example = word.slice(word.lastIndexOf('E') + 9).join('')
-        } else {
-          example = word.slice(word.lastIndexOf('E') + 9, word.lastIndexOf('T')).join('')
-        }
-        if (!(word.length === 0)) {
-          result.push({ translate: translate, example: example })
-        }
-      }
-      result.length === 0 ? reply = '沒有這個單字的資料，請搜尋其他單詞~'
-        : reply = [{
-          type: 'flex',
-          altText: 'Flex',
-          contents: {
-            type: 'carousel',
-            contents: [
-              {
-                type: 'bubble',
-                header: {
-                  type: 'box',
-                  layout: 'vertical',
-                  contents: [{
-                    type: 'text',
-                    wrap: true,
-                    text: result[0].translate
-                  }]
-                },
-                body: {
-                  type: 'box',
-                  layout: 'vertical',
-                  contents: [{
-                    type: 'text',
-                    wrap: true,
-                    text: result[0].example
-                  }]
-                },
-                styles: {
-                  header: {
-                    backgroundColor: '#83c5be'
-                  }
-                }
-              },
-              {
-                type: 'bubble',
-                header: {
-                  type: 'box',
-                  layout: 'vertical',
-                  contents: [{
-                    type: 'text',
-                    wrap: true,
-                    text: result[1].translate
-                  }]
-                },
-                body: {
-                  type: 'box',
-                  layout: 'vertical',
-                  contents: [{
-                    type: 'text',
-                    wrap: true,
-                    text: result[1].example
-                  }]
-                },
-                styles: {
-                  header: {
-                    backgroundColor: '#83c5be'
-                  }
-                }
-              }, {
-                type: 'bubble',
-                header: {
-                  type: 'box',
-                  layout: 'vertical',
-                  contents: [{
-                    type: 'text',
-                    wrap: true,
-                    text: result[2].translate
-                  }]
-                },
-                body: {
-                  type: 'box',
-                  layout: 'vertical',
-                  contents: [{
-                    type: 'text',
-                    wrap: true,
-                    text: result[2].example
-                  }]
-                },
-                styles: {
-                  header: {
-                    backgroundColor: '#83c5be'
-                  }
-                }
-              }
-            ]
-          }
-        }, {
-          type: 'audio',
-          originalContentUrl: 'https://askmeaboutexhibition.herokuapp.com/text.m4a',
-          duration: 1000
-        }]
-      // reply = (result.length === 0) ? '嗨嗨' : reply
-      event.reply(reply)
+    let reply = ''
+    switch (text) {
+      case '查新聞':
+        reply = '請輸入查詢新聞種類(商業、娛樂、健康、頭條、運動、科技)'
+        event.reply(reply)
+        break
+      case '商業': updateNews('business', event)
+        break
+      case '娛樂': updateNews('entertainment', event)
+        break
+      case '健康': updateNews('health', event)
+        break
+      case '頭條': updateNews('general', event)
+        break
+      case '運動': updateNews('sports', event)
+        break
+      case '科技': updateNews('technology', event)
+        break
+      default: updateData(url, event)
+        break
     }
-    updateData()
   } catch (error) {
     event.reply('發生錯誤')
     console.log(error)
